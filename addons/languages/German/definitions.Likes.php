a969746775027299abd8976a517037fbea79694a
Michael Roitzsch <reactorcontrol@icloud.com>
2015-03-14 19:07:20 +0100

    use dative plural form
    also fits the singular case

--- addons/languages/German/definitions.Likes.php
+++ addons/languages/German/definitions.Likes.php
@@ -9,4 +9,4 @@ $definitions["Like"] = "Gef&auml;llt mir";
 $definitions["Members Who Liked This Post"] = "Mitglieder, denen dieser Beitrag gefallen hat";
 $definitions["%s likes this."] = "Das gef&auml;llt %s.";
 $definitions["%s like this."] = "Das gef&auml;llt %s.";
-$definitions["%s others"] = "%s andere";
+$definitions["%s others"] = "%s anderen";
