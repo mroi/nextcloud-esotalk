44b4001b630b0fca2ec334b7c872e4972ad36111
Michael Roitzsch <reactorcontrol@icloud.com>
2020-06-07 19:38:54 +0200

    add route prefix explicitly
    routes do not get auto-prefixed with Nextcloud 19

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,7 +1,7 @@
 <?php
-$this->create('board_start', '')
+$this->create('board_start', '/apps/board/')
 	->actionInclude('board/appinfo/iframe.php');
 // esoTalk uses the p parameter to determine the request path
-$this->create('board_content', '{p}')
+$this->create('board_content', '/apps/board/{p}')
 	->requirements(array('p' => '.+'))
 	->actionInclude('board/appinfo/iframe.php');
